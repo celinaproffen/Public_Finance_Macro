9e587c3775f9e3fbcd31554a2d0c580dbc868aa2
DMDifino <davide.marco.difino@gmail.com>
2021-02-15 16:55:32 +0100

    Update EX1.m

--- PS6/code/EX1.m
+++ PS6/code/EX1.m
@@ -4,7 +4,7 @@ function exercise
 close all
 
 global alpha betta tetta delta replrate tau lambda 
-global retage maxage L R N_age sr 
+global retage maxage L R N N_age sr nx curv grdfac 
 
 rho = 0.05;  % 0.01
 delta = 0.05;
@@ -34,7 +34,7 @@ L = sum(L_age);      % working age pop
 R = sum(R_age);      % retired pop
 
 % Parameters for stochastic component
-opt_det = false;       
+opt_det = 1;       
 opt_ny = 2; 
 
 % cash-on-hand  grid parameters
@@ -56,10 +56,10 @@ n = length(retvec);
 assvec = zeros(n,1);
 
 % for each interest rate point, compute assets (== capital) supplied
-for i = 1:n,
+for i = 1:n
     ret = retvec(i);
     [fval, assvec(i)] = func_olg(ret);
-end;
+end
 
 % graph of K(r) = optimal demand (input choice):
 kvec = (alpha./(retvec+delta)).^(1/(1-alpha)).*L;
@@ -80,28 +80,29 @@ print -depsc equil.eps
 ret = retold;
 
 % iterate until r* s.t. => fval < tol 
-for it=1:maxit,
+for it=1:maxit
     
     % given ret, compute differene between demand and supply r
     [fval, ass, Y] = func_olg(ret);
     
     % if fval(ret) < tol, STOP 
-    if (abs(fval)<tol),
+    if (abs(fval)<tol)
         disp('convergence');
         break;
         
     % fval(ret) > tol
-    else,
+    else
         
         % update ret using df as a smoothing parameter
         ret = ret - df * fval;
         disp(['iteration #', num2str(it)]);
         disp(['guess of rate of return: ', num2str(ret)]);
-    end;
-end;
-if (it>=maxit),
+    end
+end
+
+if (it>=maxit)
     warning('no convergence');
-end;
+end
 disp(['equilibrium interest rate: ', num2str(ret)]);
 disp(['equilibrium contribution rate: ', num2str(tau)]);
 disp(['equilibrium capital output ratio: ', num2str(ass/Y)]);
@@ -111,15 +112,15 @@ end
 
 % compute the size of each cohort given min, max and survival rate sr
 % ------------------------------------------------------------------------
-function [N,L,R]=func_pop(sr,maxage,retage);
+function [N,L,R]=func_pop(sr,maxage,retage)
 
 N = zeros(maxage,1);
 L = zeros(maxage,1);
 R = zeros(maxage,1);
 N(1) = 100;
-for age=2:maxage,
+for age=2:maxage
     N(age)=N(age-1)*sr(age-1);
-end;
+end
 L = N(1:retage);
 R = N(retage+1:maxage);
 
@@ -133,7 +134,7 @@ end
 % convergence => fval < eps
 function [fval,ass,Y] = func_olg(ret)
 
-global retage maxage L R N_age replrate tau sr lambda delta
+global retage maxage L R N_age replrate tau sr  delta gridy pini pi
 
 % MPK = MC_k = interest rate + discount (from firm f.o.c.)
 mpk = ret + delta;
@@ -144,19 +145,15 @@ wage = func_firm(mpk);
 % compute pension contribution
 tau = func_pens(L,R,replrate); % == 0 w/ current calibration
 
-% compute income for each cohort
-inc_det = func_inc(wage,tau,replrate,retage,maxage);
+% compute deterministic component of income for each cohort
+det_inc = func_inc(wage,tau,replrate,retage,maxage);
 
 % compute gross saving from hh optimization problem
 % (by age, shocks, and cash at hand) 
-[sav_age, vfun] = func_hh_stoc(det_inc, ret, maxage, retage, sr, lambda);
+[gridx,gridsav,gridass,cfun,vfun] = func_hh_stoc(det_inc,ret,maxage,sr);
 
-% we want a unique saving level for each cohort:
-    % @TODO determine life-cycle profile
-    % @TODO aggregation by shock
-
-% aggregation saving(cohort=t) = sav_t  * size_t
-%ass = func_aggr(sav_age,N_age);
+% aggregation 
+[Phi,PhiAss,ass] = func_aggr_stoc(gridx, gridsav, cfun, det_inc);
 
 % compute capital level == saving => output (Y) implied by ret
 [mpk,Y] = func_mpk(ass, L);
@@ -180,7 +177,6 @@ wage = (1-alpha)*k^alpha;
 
 end
 
-
 % pension system conribution/taxation? PAYGO?
 % ------------------------------------------------------------------------
 function tau = func_pens(L,R,replrate)
@@ -192,7 +188,7 @@ end
 
 % compute (deterministic) income profile over time given wage and pension  
 % ------------------------------------------------------------------------
-function inc=func_inc(wage,tau,replrate,retage,maxage);
+function inc = func_inc(wage,tau,replrate,retage,maxage)
 
 inc=zeros(maxage,1);
 inc(1:retage)=wage*(1-tau);
@@ -211,7 +207,7 @@ mpw = ones(maxage, 1);       % marginal propensity to consume out of total wealt
 hk = zeros(maxage, 1);                 % human capital
 RF = ones(maxage, 1)+ret;
 annf = ones(maxage, 1);        % annuitization factor
-for age = maxage-1 : -1 : 1,
+for age = maxage-1 : -1 : 1
     annf(age+1) = (sr(age) + lambda * (1-sr(age)))/sr(age);
     RF(age+1) = (1+ret) * annf(age+1);
     b = (betta * sr(age) * RF(age+1)^(1-tetta))^(-1/tetta);
@@ -219,7 +215,7 @@ for age = maxage-1 : -1 : 1,
 
     % human capital: future discounted labor income
     hk(age) = (hk(age+1)+inc(age+1))/RF(age+1);
-end;
+end
 
 % forward loop: optimal decisions:
 gr_sav = zeros(maxage, 1);    % gr_sav = x-cons
@@ -231,42 +227,42 @@ ass = zeros(maxage, 1);       % finanical assets
 mpx = zeros(maxage, 1);
 
 tot_w(1) = ass(1)+inc(1)+hk(1); 
-for age = 1 : maxage,
+for age = 1 : maxage
     cons(age) = mpw(age) .* tot_w(age);
     x(age) = tot_w(age) - hk(age); 
     mpx(age)=cons(age)/x(age);
     ass(age) = x(age) - inc(age);
     gr_sav(age) = x(age) - cons(age);
     net_sav(age) = inc(age) - cons(age);
-    if age < maxage,
+    if age < maxage
         tot_w(age+1) = (tot_w(age)-cons(age))*RF(age+1);
-    end;
-end;
+    end
+end
 consgr=cons(2:end)./cons(1:end-1);
 
 % check bugdet constraint:
 df=ones(maxage,1);
 af=annf(1);     % adjustment factor accounting for annuities
-for age=1:maxage-1,
+for age=1:maxage-1
     af=af*annf(age+1);
     df(age+1)=1/(1+ret)^age/af;
-end;
+end
 bc=sum(cons.*df-inc.*df);
 if (abs(bc)>sqrt(eps))
     warning('present value budget constraint is violated');
-end;
+end
 
 end
 
 
-% household optimization problem (Stochastic)
+% NEW! household optimization problem (stochastic)
 % ------------------------------------------------------------------------
-function [gr_sav, value]=func_hh_stoc(det_inc,ret,maxage,retage,sr,lambda)
+function [gridx,gridsav,gridass,cfun,vfun]=func_hh_stoc(det_inc,ret,maxage,sr)
 
-global betta ret maxage retage sr lambda tetta opt_det opt_ny nshock nx curv grdfac
+global betta tetta opt_det opt_ny nshock nx curv grdfac pi
 
 % Compute Markov Chain parameters
-[nshock, pini, gridy,pi]=stoch_inc_proc(opt_det, opt_ny);
+[nshock, pini, gridy, pi]=stoch_inc_proc(opt_det, opt_ny);
 
 % grids and decisions rules:
 gridx = zeros(maxage,nshock,nx); % years of life * possible distinct numbers for permanent shock realizations * possible cash-at-hand-states (three dimensional!)
@@ -278,8 +274,8 @@ vpfun = zeros(nx,1); % first derivative of the value function (w.r.t.consumption
 vptrans = zeros(maxage,nshock,nx); % transformed value function in all states and ages for different cash-at-hand levels
 
 % savings grid: hold it constant:
-maxsav=grdfac;
-gridsav(2:nx)=makegrid(0.0,grdfac,nx-1,curv);
+maxsav = grdfac;
+gridsav(2:nx)=makegrid(0.0, maxsav, nx-1, curv);
 gridsav(1)=0.0;
 
 % solve final period problem
@@ -302,7 +298,7 @@ for yc=1:nshock
     cfun(maxage,yc,:)=gridx(maxage,yc,:);
     
     % assets from prevous period as implied dy CAH
-    gridass(maxage,yc,:)=(gridx(maxage,yc,:)-inc)/(1+r);
+    gridass(maxage,yc,:)=(gridx(maxage,yc,:)-inc)/(1+ret);
     
     % value function
     vfun(maxage,yc,:)=U(cfun(maxage,yc,:));
@@ -324,13 +320,13 @@ for jc = maxage-1:-1:1
         % ----------------------------------------------------------------
         for xc=2:nx
             
-            vp = zeros(ny,1);
+            vp = zeros(nshock, 1);
             
             % Solve tomorrows problem
             % ------------------------------------------------------------
            
             % loop over possible shock tomorrow
-            for ycc=1:ny
+            for ycc=1:nshock
                 
                 % income tomorrow (ASSUME == TODAY?)
                 incp1 = det_inc(jc+1)*gridy(ycc);
@@ -345,9 +341,11 @@ for jc = maxage-1:-1:1
                 end
                 
                 if ( cah>gridx(jc+1,ycc,nx) )
+                    
                     % if out of bounds simply set it to decision at nx:
                     vptr = vptrans(jc+1,ycc,nx);
                 else
+                    
                     vptr = interp1(squeeze(gridx(jc+1,ycc,:)),squeeze(vptrans(jc+1,ycc,:)),cah);
                 end
                 
@@ -385,10 +383,10 @@ for jc = maxage-1:-1:1
         %-----------------------------------------------------------------
         
         % assets implied by all x: 
-        gridass(jc,yc,:)=(gridx(jc,yc,:)-inc)/(1+r); % <= TARGET
+        gridass(jc,yc,:)=(gridx(jc,yc,:)-inc)/(1+ret);    % <= TARGET
         
         % Update vfun and vpfun
-        vpfun(:)=MUc(cfun(jc,yc,:));
+        vpfun(:)=MUc(cfun(jc,yc,:));                    % <= TARGET
         vptrans(jc,yc,:)=vpfun(:).^(-1.0/tetta);
         
         % compute value function
@@ -396,7 +394,7 @@ for jc = maxage-1:-1:1
             
             v=zeros(nshock, 1);
             
-            for ycc=1:ny
+            for ycc=1:nshock
                 
                 % income tomorrow:
                 incp1 = det_inc(jc+1)*gridy(ycc);
@@ -405,9 +403,9 @@ for jc = maxage-1:-1:1
                 cah = max(sqrt(eps),incp1+(1.0+ret)*gridsav(xc));
                 
                 % this should never be the case:
-                if ((cah+0.0001)<gridx(jc+1,ycc,1)),
+                if ((cah+0.0001)<gridx(jc+1,ycc,1))
                     warning('How can this be ?');
-                end;
+                end
                 % linear interpolation:
                 v(ycc)=func_intp(squeeze(gridx(jc+1,ycc,:)),squeeze(vfun(jc+1,ycc,:)),cah);
             end
@@ -418,25 +416,204 @@ for jc = maxage-1:-1:1
             % update Bellman equation
             vfun(jc,yc,xc)=U(cfun(jc,yc,xc))+betta*sr(jc)*expv;
             
-            % print outputs
-            % ------------------------------------------------------------
-            
-            % Saving grid == assets
-            gr_sav = gridass;
-            
-            % value function 
-            value = vfun;
-    end
+        end
     
+    end
 end
-end
+% Auxiliary functions
+% ------------------------------------------------------------------------
+
+% Interpolation 
+    function fv = func_intp(x,func,xp)
+        
+        
+        n = length(x);
+        if ( xp>x(n) ),
+            % fv = func(n);
+            fv=func_extrapol(x(n-1),x(n),func(n-1),func(n),xp);
+        elseif (xp<x(1)),
+            % fv = func(1);
+            fv=func_extrapol(x(1),x(2),func(1),func(2),xp);
+        else
+            fv = interp1(x,func,xp);
+        end;
+        
+    end
+% extrapolation function
+    function y=func_extrapol(x1,x2,y1,y2,x)
+        
+        % simple linear extrapolation
+        
+        m = (y2-y1)/(x2-x1);
+        y = y1 + m*(x-x1);
+        
+    end
+
+% makegrid
+    function grd = makegrid(x1,x2,n,c)
+    % makes curved grid according to curvature parameter c
+    scale=x2-x1;
+    grd(1)=x1;
+    grd(n)=x2;
+    for i=2:n-1
+        grd(i)=x1+scale*((i-1.0)/(n-1.0))^c;
+    end
+end 
+end 
 
 
-% Aggregation function
+% Aggregation function (deterministic)
 % ------------------------------------------------------------------------
 function ass = func_aggr(sav_age,N_age)
 
 ass = sum(sav_age.*N_age);
+
+end
+
+
+% NEW! Aggregation function (stochastic)
+% ------------------------------------------------------------------------
+function [Phi,PhiAss,ass]=func_aggr_stoc(gridx, gridsav, cfun, det_inc)
+    
+global nshock ret maxage nx pi gridy sr pini N
+    
+disp('aggregation and cross-sectional measure');
+    
+% compute total population
+totpop = sum(N);
+frac = N./totpop;
+    
+% Compute Cross sectional distributions and aggregate variables
+% --------------------------------------------------------------------
+    
+% distribution of assets conditional by age and shock
+Phi = zeros(maxage, nshock, nx);
+    
+% distribution of assets
+PhiAss = zeros(nx,1); 
+    
+% Distribution of newborns over cash at hand
+for yc = 1:nshock
+    
+    % income in current period/age:
+    inc = det_inc(1)*gridy(yc);
+    
+	% initial cash-on-hand:
+    cahini = inc;
+        
+	% find values and position of positive 
+	[vals,inds]=basefun(gridx(1,yc,:),cahini,nx);
+        
+	% compute transition 
+    Phi(1,yc,inds(1))=vals(1)*pini(yc)*frac(1);
+	Phi(1,yc,inds(2))=vals(2)*pini(yc)*frac(1);
+ 
+end
+    
+% compute transfer function
+%---------------------------------------------------------------------
+for jc=2:maxage
+        
+	% transfer function
+    TT = zeros(nshock, nx, nshock, nx);
+        
+    for xc=1:nx
+    	for yc=1:nshock
+        	for ycc=1:nshock
+                
+            	% income (wages and pensions) in current period/age:
+                inc = det_inc(jc)*gridy(yc);
+                
+                % cash on hand: x=a*(1+r)+y = s(-1)*(1+r)+y;
+                cah=inc+(1.0+ret)*gridsav(xc);
+                
+                [vals,inds]=basefun(gridx(jc,ycc,:),cah,nx);
+                
+                TT(ycc,inds(1),yc,xc)=vals(1)*pi(yc,ycc);
+                TT(ycc,inds(2),yc,xc)=vals(2)*pi(yc,ycc);
+            end  
+        end    
+    end
+end
+    
+for xc=1:nx
+    for yc=1:nshock
+        for xcc=1:nx
+            for ycc=1:nshock
+            	% transfer distribution:
+                Phi(jc,ycc,xcc)=Phi(jc,ycc,xcc)+Phi(jc-1,yc,xc)*TT(ycc,xcc,yc,xc)*sr(jc-1);
+            end
+        end
+    end
+end
+
+% Check if distribution sums to 1
+sumprob=sum(sum(sum(Phi(:,:,:))));
+    if ( ( sumprob < 0.999 ) || ( sumprob > 1.001) )
+        beep; beep; beep;
+        warning('distribution fucked up');
+    end
+    
+% Check if Grid is Big enough
+sumprob=sum(sum(Phi(:,:,nx)));
+if (sumprob > 0.001 )
+    beep; beep; beep;
+    warning('grid too small -- increase your grid');
+    pause
+end
+
+% NOTE: KEEP RET?
+
+% aggregation
+%-------------------------------------------------------------------------
+for jc=1:maxage
+    for yc=1:nshock
+        for xc=1:nx
+            
+            PhiAss(xc)=PhiAss(xc)+Phi(jc,yc,xc);
+            
+            % asset holdings = capital stock in general equilibrium
+            ass = totpop*Phi(jc,yc,xc)*gridsav(xc);
+            
+            % consumption level
+            cons = totpop*Phi(jc,yc,xc)*cfun(jc,yc,xc);
+            
+            %lab = totpop*Phi(jc,yc,xc)*gridy(yc)*epsi(jc);
+            %ret=ret+totpop*Phi(jc,yc,xc)*gridy(yc)*(1.0-epsi(jc));
+        end
+    end
+end
+
+% Basefun
+%-------------------------------------------------------------------------
+ function [vals,inds]=basefun(grid_x,x,nx)
+        % this subroutine returns the values and the indices of the two basis
+        % functions that are positive on a given x in the grid_x
+        
+        % MF function to lookup the current position
+        % I think this gives you the closest level in grid_x to x (here
+        % cah) in term of the index of grid_x where to find that cah-level
+        i=lookup(grid_x,x,0);
+        
+        if ( (i+1)>nx),
+            inds(1)=nx;
+            inds(2)=nx;
+            vals(2)=0.0;
+            vals(1)=1.0;
+        elseif (i==0),
+            inds(1)=1;
+            inds(2)=1;
+            vals(1)=1.0;
+            vals(2)=0.0;
+        else
+            inds(1)=i;
+            inds(2)=i+1;
+            dist = grid_x(i+1)-grid_x(i);
+            vals(2)=( x-grid_x(i) )/dist;
+            vals(1)=( grid_x(i+1)-x )/dist;
+        end;
+        
+ end 
 
 end
 
@@ -456,10 +633,10 @@ end
 
 % NEW - Markov chain income process
 % ------------------------------------------------------------------------
-function [ny, pini, gridy,pi]=stoch_inc_proc(opt_det, opt_ny)
+function [nshock, pini, gridy,pi]=stoch_inc_proc(opt_det, opt_ny)
 
-if (opt_det==1),
-    ny = 1;
+if (opt_det==1)
+    nshock = 1;
     pini = 1.0;
     gridy = 1.0;
     pi = 1.0;
@@ -467,7 +644,7 @@ else
     
     if (opt_ny==1)
         % number of income shocks
-        ny = 5;
+        nshock = 5;
         % transition probability
         rhoeta=0.98;
         % variance of "permanent" shock
@@ -477,7 +654,7 @@ else
         % Markov chain:
         % pi is the transition matrix of the Markov chain
         % gridy is the discretized state space of y_t
-        [pi,gridy] = markovappr(rhoeta,sqrt(vareta),2,ny);
+        [pi,gridy] = markovappr(rhoeta,sqrt(vareta),2,nshock);
 
         
         % ii) exact by eigenvector decomposition:
@@ -493,9 +670,9 @@ else
         
     else
         
-        % Alternative -- taken from Kr?ger and Ludwig (2007) using only two
+        % Alternative -- taken from Krueger and Ludwig (2007) using only two
         % states
-        ny = 2;
+        nshock = 2;
         
         % transition probability and variance
         rhoeta=0.97;
@@ -533,16 +710,16 @@ function u = U(c)
 global tetta
 
 % utility
-if (abs(tetta-1-0)<sqrt(eps)),
+if (abs(tetta-1-0)<sqrt(eps))
     u = log(c);
 else
     u = c.^(1.0-tetta)/(1.0-tetta);
-end;
+end
 end     
 function muc=MUc(c)
 global tetta
 
-% maringal utility
+% marginal utility
 muc = c.^(-tetta);
 end    
 function invut=invut(marg)
@@ -551,5 +728,6 @@ global tetta
 % invert utility for c
 invut=marg.^(-1.0/tetta);
 end    
+
 
 
